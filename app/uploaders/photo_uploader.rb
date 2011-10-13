4a88bb1f34d35feee43c5b71168c99a17fbb1499
Matheus E. Muller <hello@memuller.com>
2011-10-13 00:44:47 -0300

    Adds a scaled-down filesize.

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -17,7 +17,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
   end
 
   version :thumbnail do
-    process :resize_to_fit => [100,50]
+    process resize_to_fit: [100,50]
+  end
+
+  version :small do
+    process resize_to_fit: [300,200] 
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
